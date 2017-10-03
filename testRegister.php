bd3a4785acd2652df5b71f98eb7152ddf3d6006f
Anthony <amm87@njit.edu>
2017-10-03 11:48:01 -0400

    backend registration code done

--- testRegister.php
+++ testRegister.php
@@ -4,6 +4,6 @@
 require_once("register.php.inc");
 
 $register = new registerdb();
-$register->registerUser('test2','test');
-
+$register->registerUser('test4','123456','bobby','bob','test@test.com');
+ //echo $register->getClientId('test3').PHP_EOL;
 ?>
