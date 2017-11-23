7eef64de7e0a0eb6a773ca348ee94c3e14e91073
manish42896 <ml394@njit.edu>
2017-11-22 21:34:35 -0500

    condense to one page

--- Action.php
+++ Action.php
@@ -111,19 +111,19 @@
                 <div class="dropdown">
                     <button class="dropbtn"><b>Genre</b></button>
                     <div class="dropdown-content">
-                        <a href="Action.php"><font size="2">Action</font></a>
-                        <a href="Adventure.php"><font size="2">Adventure</font></a>
-                        <a href="Animated.php"><font size="2">Animated</font></a>
-                        <a href="Comedy.php"><font size="2">Comedy</font></a>
-                        <a href="Drama.php"><font size="2">Drama</font></a>
-                        <a href="Documentary.php"><font size="2">Documentary</font></a>
-                        <a href="Fantasy.php"><font size="2">Fantasy</font></a>
-                        <a href="Foreign.php"><font size="2">Foreign</font></a>
-                        <a href="Horror.php"><font size="2">Horror</font></a>
-                        <a href="Romance.php"><font size="2">Romance</font></a>
-                        <a href="ScienceFiction.php"><font size="2">Science Fiction</font></a>
-                        <a href="Supernatural.php"><font size="2">Supernatural</font></a>
-                        <a href="Suspense.php"><font size="2">Suspense</font></a>
+                        <a onclick="document.getElementById('demo').innerHTML = ('<?php display('Action') ?>');"><font size="2">Action</font></a>
+                        <a onclick="document.getElementById('demo').innerHTML = ('<?php display('Adventure') ?>');"><font size="2">Adventure</font></a>
+                        <a onclick="document.getElementById('demo').innerHTML = ('<?php display('Animated') ?>');"><font size="2">Animated</font></a>
+                        <a onclick="document.getElementById('demo').innerHTML = ('<?php display('Comedy') ?>');"><font size="2">Comedy</font></a>
+                        <a onclick="document.getElementById('demo').innerHTML = ('<?php display('Drama') ?>');"><font size="2">Drama</font></a>
+                        <a onclick="document.getElementById('demo').innerHTML = ('<?php display('Documentary') ?>');"><font size="2">Documentary</font></a>
+                        <a onclick="document.getElementById('demo').innerHTML = ('<?php display('Fantasy') ?>');"><font size="2">Fantasy</font></a>
+                        <a onclick="document.getElementById('demo').innerHTML = ('<?php display('Foreign') ?>');"><font size="2">Foreign</font></a>
+                        <a onclick="document.getElementById('demo').innerHTML = ('<?php display('Horror') ?>');"><font size="2">Horror</font></a>
+                        <a onclick="document.getElementById('demo').innerHTML = ('<?php display('Romance') ?>');"><font size="2">Romance</font></a>
+                        <a onclick="document.getElementById('demo').innerHTML = ('<?php display('ScienceFiction') ?>');"><font size="2">Science Fiction</font></a>
+                        <a onclick="document.getElementById('demo').innerHTML = ('<?php display('Supernatural') ?>');"><font size="2">Supernatural</font></a>
+                        <a onclick="document.getElementById('demo').innerHTML = ('<?php display('Suspense') ?>');"><font size="2">Suspense</font></a>
                     </div>
             </td>
             <td style="padding:0 15px 0 40px;">
@@ -153,41 +153,45 @@
         </tr>
     </table>
     <br /><br />
-    <font size="5" color="red">Action</font>
-    <br /><br /><br /><br />
+    <p id="demo"></p>
     <?php
-      require_once('get_host_info.inc');
-      require_once('rabbitMQLib.inc');
-      $request = array();
-      $request['type'] = "genre";
-      $request['genre'] = "Action";
-      $client = new rabbitMQClient("testRabbitMQ.ini", "testServer");
-      $response = $client->send_request($request);
-      $r = json_decode($response, true);
-      echo "<table>";
-      $counter = 0;
-      foreach ($r as $movie)
-      {
-        if ($counter === 0)
-        {
-          echo "<tr>";
-        }
-        else if ($counter === 4)
-        {
-          echo "</tr>";
-          $counter = 0;
-        }
-        echo "<td>";
-        $path = "http://image.tmdb.org/t/p/w185/".$movie["imagePath"];
-        echo "<a href='Forums.php?type=2&movieid=".$movie["movieId"]."'><img src='$path'/></a><br>";
-        echo $movie['title']."<br>";
-        echo $movie['releaseDate'];
-        echo "</td>";
-        
-        $counter++;
-      }
-      echo "</table>";
-      $payload = json_encode($response);
+    function display($value)
+    {
+    echo $value;
+    echo "<br><br><br><br>";
+    require_once('get_host_info.inc');
+    require_once('rabbitMQLib.inc');
+    $request = array();
+    $request['type'] = "genre";
+    $request['genre'] = $value;
+    $client = new rabbitMQClient("testRabbitMQ.ini", "testServer");
+    $response = $client->send_request($request);
+    $r = json_decode($response, true);
+    echo "<table>";
+    $counter = 0;
+    foreach ($r as $movie)
+    {
+    if ($counter === 0)
+    {
+        echo "<tr>";
+    }
+    else if ($counter === 4)
+    {
+        echo "</tr>";
+        $counter = 0;
+    }
+    echo "<td>";
+    $path = "http://image.tmdb.org/t/p/w185/".$movie["imagePath"];
+    echo "<a href='Forums.php?type=2&movieid=".$movie["movieId"]."'><img src='$path'/></a><br>";
+    echo $movie['title']."<br>";
+    echo $movie['releaseDate'];
+    echo "</td>";
+    
+    $counter++;
+    }
+    echo "</table>";
+    $payload = json_encode($response);
+    }
     ?>
     <footer>
         <center>
