c932d6dd24e4d6c4372dbaea7f1236719be0897f
shannon <sh385@njit.edu>
2017-12-02 14:20:08 -0500

    Install, rollback, and package scripts completed.

--- install.sh
+++ install.sh
@@ -1,34 +1,24 @@
 #!/bin/sh
-echo "Enter IP:"
-read Address
-echo "Enter source path:"
-read Source
-echo "Enter file name:"
-read FileName
-echo "Enter version:"
-read Version
+running=1
+while [ $running -eq 1 ]
+do
+    echo "Enter machine IP address to install to:"
+    read ip_address
 
-if ! [ -e $HOME/bundles ]
-then
-mkdir $HOME/bundles
-fi
-
-if ! [ -e $HOME/bundles/website-$Version ]
-then
-mkdir $HOME/bundles/website-$Version
-fi
-
-scp -r $Address:$Source/$FileName $HOME/bundles
-tar -xvzf $HOME/bundles/$FileName -C $HOME/bundles/website-$Version
+    if [ $ip_address = "stop" ]
+    then
+        running=0
+    else
+        ssh -t $ip_address "scp -r anthony@192.168.1.151:/home/anthony/bundles/$1 ~/bundles; 
+        mkdir ~/bundles/$2;
+        tar -xvzf ~/bundles/$1 -C ~/bundles/$2; 
+        sudo rm /var/www/html/it490project;
+        sudo ln -s ~/bundles/$2 '/var/www/html/it490project';
+	sudo service apache2 restart;
+	sudo service apache2 reload"
+    fi
+done
 
 
-if [ -e "/var/www/html/it490project" ]
-then
-sudo rm "/var/www/html/it490project" 
 
-sudo ln -s $HOME/bundles/website-$Version "/var/www/html/it490project" 
-
-fi
-
-sudo service apache2 restart
 
--- rollback.sh
+++ rollback.sh
@@ -1,23 +1,26 @@
 #!/bin/sh
 
-
-ls $HOME/bundles
-
-echo "Which version would like you to rollback to?"
-read Version
-
-
-if [ -e $HOME/bundles/$Version ]
-then
+running=1
+while [ $running -eq 1 ]
+do
+    echo "Enter IP address of a server to rollback:"
+    read ip_address
     
-    sudo rm "/var/www/html/it490project"
-    sudo ln -s $HOME/bundles/$Version "/var/www/html/it490project" 
-
-    sudo service apache2 restart
-    sudo service apache2 reload 
-    
-else 
-    echo "The version $Version does not exist. Please try again."
-    exit 
-    
-fi 
+    if [ $ip_address = "stop" ]
+    then
+        running=0
+    else
+        ssh -t $ip_address "ls ~/bundles"
+        echo "Which version would like you to rollback to?"
+        read version
+        if [ $version = "stop" ]
+        then 
+            running=0
+        else
+            ssh -t $ip_address "sudo rm /var/www/html/it490project;
+            sudo ln -s ~/bundles/$version /var/www/html/it490project;
+            sudo service apache2 restart;
+            sudo service apache2 reload;"
+        fi
+    fi
+done
