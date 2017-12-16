67cf1e7f2d558da9a7afe3d25d4e8716a323d9f5
shannon <sh385@njit.edu>
2017-12-16 11:54:25 -0500

    Google places api implemented.

--- LatestTrailers.php
+++ LatestTrailers.php
@@ -141,7 +141,14 @@
         </tr>
     </table>
     <br /><br />
-    <p id="demo"><font size="5" color="red">Latest Trailers</font></p>
+    <p id="demo"><font size="5" color="red">Latest Trailers</font> 
+    <br><br>
+    <table stye="width: 60%; height:60%;">
+        <?php
+            require_once("google.php");
+        ?>
+    </table>
+    </p>
     
     <?php
     /**
